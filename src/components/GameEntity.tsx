5999cb11cb4c75a833cab67fbdebb2694aff74dc
Son Thanh Vo <sonvo95@outlook.com>
2021-09-07 14:29:03 +0200

    feat: add damage indicator

--- src/components/GameEntity.tsx
+++ src/components/GameEntity.tsx
@@ -1,9 +1,18 @@
 import Entity from '../service/Entity';
+import { usePreviousValue } from '../utils/hooks/usePreviousValue';
 
 type GameEntityProps = {
   entity: Entity;
 };
 
 export default function GameEntity({ entity }: GameEntityProps): JSX.Element {
-  return <div dangerouslySetInnerHTML={{ __html: entity.getAvatar() }}></div>;
+  const previousHealth = usePreviousValue(entity.getHealth());
+  const recentDamage = previousHealth - entity.getHealth();
+
+  return (
+    <div className="entity">
+      <div dangerouslySetInnerHTML={{ __html: entity.getAvatar() }}></div>
+      <p className="entity-damage-received">{recentDamage}</p>
+    </div>
+  );
 }
