92ebe674fc2056528ad99c9328b158e8fff2117d
Son Thanh Vo <sonvo95@outlook.com>
2021-09-10 11:05:50 +0200

    feat: add server

---
+++ server.js
@@ -0,0 +1,57 @@
+const fastify = require("fastify")({ logger: true });
+
+fastify.addSchema({
+  $id: "PlayerData",
+  type: "object",
+  required: ["attempts", "round", "stats"],
+  properties: {
+    attempts: { type: "number" },
+    round: { type: "number" },
+    stats: {
+      type: "object",
+      required: ["damage", "health"],
+      properties: {
+        damage: { type: "number" },
+        health: { type: "number" },
+      },
+    },
+  },
+});
+
+fastify.get("/", async (_, reply) =>
+  reply.send({ statusCode: 200, data: fastify.getSchemas() })
+);
+
+const playerData = {};
+
+fastify.get("/:player", async (request, reply) => {
+  const player = request.params.player;
+  if (!playerData.hasOwnProperty(player)) {
+    return reply.code(404).send({
+      statusCode: 404,
+      error: "Bad Request",
+      message: "No player found with requested id",
+    });
+  }
+  return reply.send({ statusCode: 200, data: playerData[player] });
+});
+
+fastify.put(
+  "/:player",
+  { schema: { body: { $ref: "PlayerData#" } } },
+  async (request, reply) => {
+    const player = request.params.player;
+    playerData[player] = request.body;
+    return reply.send({ statusCode: 200, data: playerData[player] });
+  }
+);
+
+const start = async () => {
+  try {
+    fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
